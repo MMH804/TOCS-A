67e7ae76b0ec25a23fad77870df8c103bbe33c38
MMH804 <131004371+MMH804@users.noreply.github.com>
2024-05-14 09:46:56 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# Define a list of numbers
+numbers = [4, 2, 7, 1, 9, 3]
+
+# Sort the list in ascending order
+sorted_numbers = sorted(numbers)
+
+# Print the sorted list
+print("Sorted numbers:", sorted_numbers)
